609fd1c014f0bbfa777ccc7eeea2a7ace4c10f3b
Brad Campbell <bradjc5@gmail.com>
2025-05-21 15:37:13 -0400

    kernel: process_array: add docs

--- kernel/src/process_array.rs
+++ kernel/src/process_array.rs
@@ -3,11 +3,28 @@
 // Copyright Tock Contributors 2025.
 
 //! Data structure for storing `Process`es.
+//!
+//! Many Tock boards store a fixed-length array of process control blocks
+//! (PCB) for easy management and traversal of running processes. The
+//! `ProcessArray` type facilitates this.
+//!
+//! The general type for the process array abstraction is
+//! `[&Process; NUM_PROCS]`. That is, the array is only sized to store
+//! references to each PCB. The actual PCB is allocated in the process's
+//! allocated memory.
 
 use crate::process;
 use core::cell::Cell;
 
 /// Represents a slot for a process in a [`ProcessArray`].
+///
+/// A slot can be empty (`None`), or hold a reference to a
+/// [`Process`](process::Process).
+///
+/// The `ProcessSlot` type is useful for allowing slices of processes without
+/// knowing the fixed number of processes, or being templated on `NUM_PROCS`.
+/// That is, interfaces can use `[ProcessSlot]` to just use an array of process
+/// slots.
 #[derive(Clone)]
 pub struct ProcessSlot {
     /// Optionally points to a process.
@@ -15,12 +32,13 @@ pub struct ProcessSlot {
 }
 
 impl ProcessSlot {
-    /// Return the underlying [process::Process] if and only if [self]
-    /// represents an active process.
+    /// Return the underlying [`process::Process`] if the slot contains a
+    /// process.
     pub fn get_active(&self) -> Option<&'static dyn process::Process> {
         self.proc.get()
     }
 
+    /// Check if the slot contains a process with a matching process ID.
     pub fn is_valid_for(&self, identifier: usize) -> bool {
         match self.proc.get() {
             Some(process) => process.processid().id() == identifier,
@@ -29,7 +47,7 @@ impl ProcessSlot {
     }
 }
 
-/// Storage for an array of `Process`es.
+/// Storage for a fixed-size array of `Process`es.
 pub struct ProcessArray<const NUM_PROCS: usize> {
     processes: [ProcessSlot; NUM_PROCS],
 }
